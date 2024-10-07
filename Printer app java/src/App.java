0723d2975c57302fa671046c551c41c871d6e7ab
Madhavi <nwwmadhi12@gmail.com>
2024-10-07 11:13:38 +0300

    Initial commit

---
+++ Printer app java/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Printer app");
+        System.out.println("This app is made by");
+
+        String creator = "Madhavi";
+        System.out.println("This app is made by: " + creator);
+
+        int number1 = 10;
+        int number2 = 20;
+        System.out.println(number1);
+    }
+}
+
