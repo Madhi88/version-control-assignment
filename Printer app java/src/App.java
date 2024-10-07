9c9876d76c5dfdd55b89c9e49306b4019865cabe
Madhavi <nwwmadhi12@gmail.com>
2024-10-07 11:50:29 +0300

    changed the nuber1 and nuber2 to check some mathematical functions

--- Printer app java/src/App.java
+++ Printer app java/src/App.java
@@ -6,13 +6,22 @@ public class App {
         String creator = "Madhavi";
         System.out.println("This app is made by: " + creator);
 
-        int number1 = 06;
-        int number2 = 02;
+        int number1 = 99;
+        int number2 = 04;
         // System.out.println(number1);
         System.out.println("The value of number1 is: " + number1);
         System.out.println("The value of number2 is: " + number2);
         int multiplication = number1 * number2;
         System.out.println(number1 + "*" + number2 +"=" + multiplication);
+
+        int addition = number1 + number2;
+        System.out.println(number1 + "+" + number2 +"=" + addition);
+
+        int subtraction = number1 - number2;
+        System.out.println(number1 + "-" + number2 +"=" + subtraction);
+
+        double division = (double) number1 / number2;
+        System.out.println(number1 + "/" + number2 +"=" + division);
     }
 }
 
